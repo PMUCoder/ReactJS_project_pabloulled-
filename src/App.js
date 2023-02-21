5a663380d972ea75adc52c5cd1f212c4420323ec
PMUCoder <pabloulled@hotmail.com>
2023-02-21 02:05:41 -0300

    progress2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App () {
   return (
     <div>
       <Navibar/>
+      <h3 className="itemListContainerStyle">ItemListContainer</h3>
+      <hr></hr>
       <ItemListContainer Product="Paraguas" Description="Para dos personas" Price= "3.000,00.-"/>
+      <ItemListContainer Product="Zapatillas" Description="Tenis talle 42" Price= "25.000,00.-"/>
+      <ItemListContainer Product="Raqueta" Description="250grs Oversize" Price= "56.000,00.-"/>
     </div> 
   )
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,14 @@
 import './ItemListContainer.scss'
 
 export const ItemListContainer = ( {Product, Description, Price} ) => {
+    //el greeting ya esta desestructurado 
     return (
-        <ul className="contenedor">
-            <li>Producto: {Product}</li>
-            <li>Descripción: {Description}</li>
-            <li>Precio: {Price}</li>
-        </ul>
-        
+        <div>
+            <ul className="contenedor">
+                <li>Producto: {Product}</li>
+                <li>Descripción: {Description}</li>
+                <li>Precio: {Price}</li>
+            </ul>
+        </div>
     )
 }
